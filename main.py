8257e4833011b55a48f7a705a5cbfe5c9f6b86aa
Prkrr20 <230140@lasallehs.net>
2020-11-09 14:19:06 +0000

    2 inputs, 2 outputs. still works

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-import nn
 import numpy as np
+import nn as nut
 from itertools import count
 
-# wfn = weight file name. where the weights are saved/loaded
-wfn = "weights.npy"
+wfn0 = "weights0.npy"
+wfn1 = "weights1.npy"
+
+tinputs = np.array([[1, 1],
+                    [1, 0],
+                    [0, 1],
+                    [0, 0]])
+
+toutputs0 = np.array([[1],[1],[1],[0]])
+toutputs1 = np.array([[0],[0],[0],[1]])
 
 try:
-	with open(wfn, "rb") as f:
-		weights = np.load(f)
+    with open(wfn0, "rb") as f:
+        weights0 = np.load(f)
 except:
-	weights = np.array([[.1],
-											[.2]])
+    weights0 = np.array([[.1],[.2]])
 
-tinputs = np.array([[1, 1],[1, 0],[0, 1],[0, 0]])
-
-toutputs = np.array([[1],[1],[1],[0]])
-
-
-net = nn.Net(wfn, weights, tinputs, toutputs)
-
-print(net)
-
-for i in count(0):
-	net.train()
-	in1 = net.run(np.array([1, 1]))
-	in2 = net.run(np.array([0, 1]))
-	in3 = net.run(np.array([1, 0]))
-	in4 = net.run(np.array([0, 0]))
+try:
+    with open(wfn1, "rb") as f:
+        weights1 = np.load(f)
+except:
+    weights1 = np.array([[.1],[.2]])
 
 
-	print(f"\n--------------------\n\nNut: {i+1}\n\n{in1}\n\n{in2}\n\n{in3}\n\n{in4}\n")
+net0 = nut.NN(tinputs, toutputs0, weights0, wfn0)
+net1 = nut.NN(tinputs, toutputs1, weights1, wfn1)
+
+
+for a in count(0):
+    net0.train()
+    net1.train()
+
+    in1 = np.array([1, 1])
+    in2 = np.array([1, 0])
+    in3 = np.array([0, 1])
+    in4 = np.array([0, 0])
+
+    out1 = np.array([net1.run(in1), net0.run(in1)])
+    out2 = np.array([net1.run(in2), net0.run(in2)])
+    out3 = np.array([net1.run(in3), net0.run(in3)])
+    out4 = np.array([net1.run(in4), net0.run(in4)])    
+    
+    print(f"\n--------------------\n\ngen: {a+1}\n\n{out1}\n\n{out2}\n\n{out3}\n\n{out4}")
--- nn.py
+++ nn.py
@@ -1,55 +1,46 @@
 import numpy as np
 
-# activation function. used to get output
-sigmoid = lambda x : 1/(1+np.exp(-x))
-# derivitive of the activation function. used to change weights
-sigder = lambda x : sigmoid(x)*(1-sigmoid(x))
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
-class Net:
-	# runs when a new Net is created
-	def __init__(self, wfn, weights, tinputs, toutputs, trainlen=30000, bias=.3, lr=.05):
-		self.wfn = wfn
-		self.weights = weights
-		self.tinputs = tinputs
-		self.toutputs = toutputs
-		self.trainlen = trainlen
-		self.bias = bias
-		self.lr = lr
+def sigder(x):
+    return sigmoid(x)*(1-sigmoid(x))
 
-	# runs when a Net is interracted with eg (print(Net))
-	def __repr__(self):
-		return f"{self.weights}"
+class NN:
+    def __init__(self, tinputs, toutputs, weights, wfn, trainlen=30000, bias=.3, lr=.05):
+        self.tinputs = tinputs
+        self.toutputs = toutputs
+        self.weights = weights
+        self.wfn = wfn
+        self.bias = bias
+        self.lr = lr
+        self.trainlen = trainlen
+    
+    def __repr__(self):
+        return f"{self.weights}"
 
 
-	# training function
-	def train(self):
-		for j in range(self.trainlen):
-			self.inputs = self.tinputs
+    def train(self):
+        for a in range(self.trainlen):
+            self.inputs = self.tinputs
 
-			# takes the sigmoid of the dot product (matrix multiplication) of the inputs and weights
-			self.inp = np.dot(self.inputs, self.weights)
-			self.out = sigmoid(self.inp)
+            self.inp = np.dot(self.inputs, self.weights)
+            self.out = sigmoid(self.inp)
 
-			# calculates how wrong the guess was
-			self.error = self.out - self.toutputs
+            self.error = self.out - self.toutputs
+            self.derror_dout = self.error
+            self.dout_din = sigder(self.out)
+            self.deriv = self.derror_dout * self.dout_din
 
-			# calculates the derivitive of the function (inputs -> outputs)
-			self.derror_dout = self.error
-			self.dout_din = sigder(self.out)
-			self.deriv = self.derror_dout * self.dout_din
-			# transposes the inputs
-			self.inputs = self.tinputs.T
-			self.derivfin = np.dot(self.inputs, self.deriv)
+            self.inputs = self.tinputs.T
+            self.derivfin = np.dot(self.inputs, self.deriv)
 
-			# finally, update the weights and the bias
-			self.weights -= self.lr * self.derivfin
-			for i in self.deriv:
-				self.bias -= i * self.derivfin
-		
-		# save the updated weights
-		with open(self.wfn, "wb") as f:
-			np.save(f, self.weights)
+            self.weights -= self.derivfin * self.lr
 
-	def run(self, point):
-		return sigmoid(np.dot(point, self.weights))
-		
+            for b in self.deriv:
+                self.bias -= b * self.lr
+        with open(self.wfn, "wb") as f:
+            np.save(f, self.weights)
+
+    def run(self, point):
+        return sigmoid(np.dot(point, self.weights))
